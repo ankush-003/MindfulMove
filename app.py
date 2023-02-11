ad520b0e49cc99c8d497de7f9215ee96939d52a6
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-02-11 13:15:19 +0530

    loaded model

--- app.py
+++ app.py
@@ -3,18 +3,26 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, R
 import cv2
 import mediapipe as mp
 import tensorflow as tf
+import numpy as np
+import tools
 mp_drawing = mp.solutions.drawing_utils
 # app
 app = Flask(__name__)
 global stop 
 stop = False
 #loading model
-model = tf.keras.models.load_model('./models/action-2.h5')
+# model = tf.keras.models.load_model('./models/action-2.h5')
 
 class Camera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
         self.pose = mp.solutions.pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        self.sequence = []
+        self.posture = []
+        self.predictions = []
+        self.actions = np.array(['vrikshasana'])
+        self.threshold = 0.5
+        self.current_action = ''
         
     def __del__(self):
         self.video.release()
@@ -32,11 +40,50 @@ class Camera(object):
             return jpeg.tobytes()
         else:
             return None
+    
+    def model_predict(self):
+        model = tf.keras.models.load_model('./models/action-2.h5')
+        ret, frame = self.video.read()
+        if ret:
+            image, results = tools.mediapipe_detection(frame, self.pose)
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+            
+            keypoints = tools.extract_keypoints(results)
+            self.sequence.append(keypoints)
+            self.sequence = self.sequence[-30:]
+            
+            if len(self.sequence) == 30:
+                res = model.predict(np.expand_dims(self.sequence, axis=0))[0]
+                print(self.actions[np.argmax(res)])
+                self.predictions.append(np.argmax(res))
+                
+                #visualization
+                if np.unique(self.predictions[-10:])[0] == np.argmax(res):
+                    if res[np.argmax(res)] > self.threshold:
+                        self.current_action = self.actions[np.argmax(res)]
+                
+                image = tools.prob_viz(res, self.actions, image, colors=[(0, 255, 0) if x == np.argmax(res) else (0, 0, 255) for x in range(len(self.actions))])
+                
+            cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+            cv2.putText(image, 'ACTION: ' + self.current_action, (3, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # image.flags.writeable = False
+            # results = self.pose.process(image)
+            # image.flags.writeable = True
+            # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # mp_drawing.draw_landmarks(image, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+            ret, jpeg = cv2.imencode('.jpg', image)
+            return jpeg.tobytes()
+        else:
+            return None
+     
+
         
 # genereate frame
 def gen(camera):
     while not stop:
-        frame = camera.get_frame()
+        # frame = camera.get_frame()
+        frame = camera.model_predict()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
     camera.__del__()        
---
+++ tools.py
@@ -0,0 +1,24 @@
+import cv2
+import mediapipe as mp
+import tensorflow as tf
+import numpy as np
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    return np.concatenate([pose])
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0, 60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    
+    return output_frame
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
